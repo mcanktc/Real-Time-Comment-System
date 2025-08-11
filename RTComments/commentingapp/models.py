bf59a357d9543bd22ad34d6ab17f1288572bed8f
Mehmetcan KATIRCI <mcanktc06@gmail.com>
2025-08-11 12:30:54 +0300

    feat(comments): add Comment model and serializer

--- RTComments/commentingapp/models.py
+++ RTComments/commentingapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Comment(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=750)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_At = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user
---
+++ RTComments/commentingapp/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Comment
+import bleach
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["__all__"]
+        read_only_fields = ['user']
+
+    def validate_text(self, value):
+        clean_text = bleach.clean(value)
+        return clean_text
